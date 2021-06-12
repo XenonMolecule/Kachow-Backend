9266e2ad881ad73a00f363f1880bf5edb9ec3169
Michael Ryan <michael_ryan_2000@yahoo.com>
2021-06-12 01:05:25 -0400

    Connected to React app

--- app.js
+++ app.js
@@ -9,18 +9,20 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Consider uncommenting if using react routing
+// app.use(function(req, res, next) {
+//   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
